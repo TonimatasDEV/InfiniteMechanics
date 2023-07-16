719f2d0e58ffa2e59943d6137319683409043efe
TonimatasDEV <amatascubero@gmail.com>
2023-07-16 18:30:40 +0200

    Initial commit

---
+++ src/main/java/net/tonimatasdev/tdindustrialnetworkcraft/TD_IndustrialNetworkCraft.java
@@ -0,0 +1,17 @@
+package net.tonimatasdev.tdindustrialnetworkcraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TD_IndustrialNetworkCraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
